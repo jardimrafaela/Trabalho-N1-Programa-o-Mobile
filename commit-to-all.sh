65279049ab0e52bfba913a8621a1305630de6cd3
rafaela.jardim <rafaela.jardim@contavoltz.com>
2023-05-30 20:27:44 -0300

    adding this diretory update to all branches

---
+++ commit-to-all.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+branches=("master" "branch-2" "branch-3")
+file_name="C:\Users\rgj4\Documents\repo github test\k8s - Copia"
+commit_message="adding this diretory update to all branches"
+
+for branch in "${branches[@]}"; do
+    # Checkout the branch
+    git checkout "$branch"
+
+    # Insert the file
+    cp -r "$file_name" .
+
+    # Add the file
+    git add "$(basename "$file_name/*")"
+
+    # Commit the changes
+    git commit -m "$commit_message"
+
+    # Push the changes to the remote branch
+    git push origin "$branch"
+done
+
